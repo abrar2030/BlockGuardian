0cfe22cf4a84b985451e31e4e694ebfb5e774d89
Abrar Ahmed <abrarahmedny@gmail.com>
2026-08-10 14:22:14 +0500

    Implement updates for better performance

--- code/blockchain/hardhat.config.js
+++ code/blockchain/hardhat.config.js
@@ -1,15 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
-  networks: {
-    goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-  },
+
   paths: {
     artifacts: "./artifacts",
     cache: "./cache",
